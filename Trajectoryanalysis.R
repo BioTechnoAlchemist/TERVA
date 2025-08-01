1548693fa3bfb656df77742ee9203ac0b2d818f8
Lea M <40102512+BioTechnoAlchemist@users.noreply.github.com>
2025-08-01 13:03:59 +0300

    Update Trajectoryanalysis.R

--- Trajectoryanalysis.R
+++ Trajectoryanalysis.R
@@ -1,87 +1,19 @@
 ### Trajectory analysis of Fibroblasts in PVAT and eWAT ####
 
-#remotes::install_github("dynverse/dynfeature")
-#remotes::install_github("dynverse/dynplot")
-#remotes::install_github("elolab/Totem")
+setwd("~/Desktop/TERVA/TERVA2DATA/")
+library(Totem)
+library(SingleCellExperiment)
+library(cowplot)
+library(dyndimred)
+library(S4Vectors)
+library(Seurat)
+library(ggplot2)
 
-suppressMessages(library(Totem))
-suppressMessages(library(SingleCellExperiment))
-suppressMessages(library(cowplot))
-suppressMessages(library(dyndimred))
-suppressMessages(library(S4Vectors))
-
-load("TERVA_fibro_Totem.RData")
+load("TERVA_fibro_Totem.RData") #This has been created from the full integrated TERVA2_harmony data (seurat object), by just subsetting the fibroblasts.
 
 Idents(Fibroblasts) <- "tissue_id"
 PVAT <- subset(Fibroblasts, idents = "PVAT")
 eWAT <- subset(Fibroblasts, idents = "eWAT")
-
-set.seed(42)
-counts <- Fibroblasts[["RNA"]]@counts
-logcounts <- Fibroblasts[["RNA"]]@data
-clusters <- list(as.character(Fibroblasts@meta.data$celltype))
-names(clusters[[1]]) <- colnames(Fibroblasts)
-fibrodata <- c(counts, logcounts, clusters)
-y <- c("counts", "logcounts", "clusters")
-names(fibrodata) <- paste0(y)
-fibrodata["counts"] <- t(fibrodata$counts)
-fibrodata["logcounts"] <- t(fibrodata$logcounts)
-
-
-sce <- SingleCellExperiment(assays = list(counts = t(fibrodata$counts),
-                                          logcounts = t(fibrodata$logcounts),
-                                          colData = DataFrame()))
-sce <- PrepareTotem(sce)
-
-sce <- RunDimRed(object = sce,
-                 dim.red.method = "lmds",
-                 dim.red.features = NULL,
-                 dim.reduction.par.list = list(ndim=5))
-
-
-#Alternative
-
-set.seed(42)
-counts <- Fibroblasts[["RNA"]]@counts
-logcounts <- Fibroblasts[["RNA"]]@data
-clusters <- list(as.character(Fibroblasts@meta.data$celltype))
-names(clusters[[1]]) <- colnames(Fibroblasts)
-fibrodata <- c(counts, logcounts, clusters)
-y <- c("counts", "logcounts", "clusters")
-names(fibrodata) <- paste0(y)
-fibrodata["counts"] <- t(fibrodata$counts) # this is fine, but you don't need to transpose if you use double square bracket like this: fibrodata[["counts"]] <- fibrodata$counts
-fibrodata["logcounts"] <- t(fibrodata$logcounts) # same here
-
-
-sce <- SingleCellExperiment(assays = list(counts = t(fibrodata$counts), # this is fine, but if you don't transpose above and use square brackets, you don't need to transpose here
-                                          logcounts = t(fibrodata$logcounts)))
-sce <- PrepareTotem(sce)
-
-sce <- RunDimRed(object = sce,
-                 dim.red.method = "lmds",
-                 dim.red.features = NULL,
-                 dim.reduction.par.list = list(ndim=5))
-
-
-dim_red <- dimred_mds(fibrodata$logcounts, ndim = 2)
-
-sce <- RunClustering(sce,
-                     k.range = 3:20,
-                     min.cluster.size = 5,
-                     N.clusterings=10000)
-
-VizCellConnectivity(sce, viz.dim.red = dim_red)
-
-sce <- SelectClusterings(sce, selection.method = 5,
-                         selection.N.models = 10,
-                         selection.stratified=FALSE,
-                         prior.clustering = fibrodata$clusters)
-
-trajnames <- ReturnTrajNames(sce) #Check names
-clustering_name <- ReturnTrajNames(sce)[1]
-clustering <- ReturnClustering(sce,clustering_name)
-VizClustering(sce, clustering = clustering, viz.dim.red = dim_red) # there isn't a 'clustering.names' parameter
-VizClustering(sce, clustering = fibrodata$clusters, viz.dim.red = dim_red) 
 
 #eWAT
 
@@ -93,11 +25,73 @@ names(clusters[[1]]) <- colnames(eWAT)
 fibrodata <- c(counts, logcounts, clusters)
 y <- c("counts", "logcounts", "clusters")
 names(fibrodata) <- paste0(y)
-fibrodata["counts"] <- t(fibrodata$counts) # this is fine, but you don't need to transpose if you use double square bracket like this: fibrodata[["counts"]] <- fibrodata$counts
-fibrodata["logcounts"] <- t(fibrodata$logcounts) # same here ### OWN COMMENT: I actually tested this suggestion and dimred_mds was significantly slower (didn't wait it to go through, because the double-transpose is so much quicker)
+fibrodata["counts"] <- t(fibrodata$counts) 
+fibrodata["logcounts"] <- t(fibrodata$logcounts) # It was suggested that I could use double square brackets and then not double transpose, but I tested this suggestion and dimred_mds was significantly slower (didn't wait it to go through, because the double-transpose is so much quicker)
+
+sce <- SingleCellExperiment(assays = list(counts = t(fibrodata$counts), 
+                                          logcounts = t(fibrodata$logcounts)))
+sce <- PrepareTotem(sce)
+
+sce <- RunDimRed(object = sce,
+                 dim.red.method = "lmds",
+                 dim.red.features = NULL,
+                 dim.reduction.par.list = list(ndim=5))
+
+dim_red <- dimred_mds(fibrodata$logcounts, ndim = 2)
+
+sce <- RunClustering(sce,
+                     k.range = 3:20,
+                     min.cluster.size = 5,
+                     N.clusterings=10000)
+
+VizCellConnectivity(sce, viz.dim.red = dim_red)
+
+sce <- SelectClusterings(sce, selection.method = 5,
+                         selection.N.models = 10,
+                         selection.stratified=FALSE,
+                         prior.clustering = fibrodata$clusters)
+
+trajnames <- ReturnTrajNames(sce) #Check names, note that these might differ if run with different package versions
+clustering_name <- ReturnTrajNames(sce)[1] #If you don't agree with the first trajectory, state here a specific one. If there's only little difference between the estimated clustering results, then it should be fine to take the first one "arbitrarily".
+clustering <- ReturnClustering(sce,clustering_name)
+VizClustering(sce, clustering = clustering, viz.dim.red = dim_red) 
+#VizClustering(sce, clustering = fibrodata$clusters, viz.dim.red = dim_red) #This can take a long time.
+
+#ReturnMSTNetwork(sce,clustering.name = clustering_name) #check the MST network
+
+sce <- RunSmoothing(sce) 
+ReturnTrajNames(sce)
+VizSmoothedTraj(sce,
+                traj.names = ReturnTrajNames(sce),
+                viz.dim.red = dim_red,plot.pseudotime = FALSE)
+
+ReturnSmoothedTrajNetwork(sce,clustering.name = clustering_name)
+
+#sce <- ChangeTrajRoot(sce,traj.name=clustering_name, root.cluster = 1) #Change the root cluster if necessary
+#VizSmoothedTraj(sce,
+#                traj.names = "3.133",
+#                viz.dim.red = dim_red,plot.pseudotime = FALSE)
+
+#With pseudotime
+VizSmoothedTraj(sce,
+                traj.names = clustering_name,
+                viz.dim.red = dim_red,plot.pseudotime = TRUE)
+
+# PVAT ####
+
+set.seed(42)
+counts <- PVAT[["RNA"]]@counts
+logcounts <- PVAT[["RNA"]]@data
+clusters <- list(as.character(PVAT@meta.data$celltype))
+names(clusters[[1]]) <- colnames(PVAT)
+fibrodata <- c(counts, logcounts, clusters)
+y <- c("counts", "logcounts", "clusters")
+names(fibrodata) <- paste0(y)
+fibrodata["counts"] <- t(fibrodata$counts) 
+fibrodata["logcounts"] <- t(fibrodata$logcounts) 
 
 
-sce <- SingleCellExperiment(assays = list(counts = t(fibrodata$counts), # this is fine, but if you don't transpose above and use square brackets, you don't need to transpose here
+sce <- SingleCellExperiment(assays = list(counts = t(fibrodata$counts), 
                                           logcounts = t(fibrodata$logcounts)))
 sce <- PrepareTotem(sce)
 
@@ -122,12 +116,12 @@ sce <- SelectClusterings(sce, selection.method = 5,
                          prior.clustering = fibrodata$clusters)
 
 trajnames <- ReturnTrajNames(sce) #Check names
-clustering_name <- ReturnTrajNames(sce)[1]
+clustering_name <- ReturnTrajNames(sce)[1] 
 clustering <- ReturnClustering(sce,clustering_name)
-VizClustering(sce, clustering = clustering, viz.dim.red = dim_red) # there isn't a 'clustering.names' parameter
-VizClustering(sce, clustering = fibrodata$clusters, viz.dim.red = dim_red) 
+VizClustering(sce, clustering = clustering, viz.dim.red = dim_red) 
+#VizClustering(sce, clustering = fibrodata$clusters, viz.dim.red = dim_red) # This can take a long time.
 
-#ReturnMSTNetwork(sce,clustering.name = "") #check the MST network
+#ReturnMSTNetwork(sce,clustering.name = "") #
 
 sce <- RunSmoothing(sce) 
 ReturnTrajNames(sce)
@@ -135,22 +129,16 @@ VizSmoothedTraj(sce,
                 traj.names = ReturnTrajNames(sce),
                 viz.dim.red = dim_red,plot.pseudotime = FALSE)
 
-ReturnSmoothedTrajNetwork(sce,clustering.name = "3.50")
+ReturnSmoothedTrajNetwork(sce,clustering.name = clustering_name)
 
-#sce <- ChangeTrajRoot(sce,traj.name="3.133", root.cluster = 1) #Change the root cluster if necessary
+#sce <- ChangeTrajRoot(sce,traj.name=clustering_name, root.cluster = 1) #Change the root cluster if necessary
 #VizSmoothedTraj(sce,
-#                traj.names = "3.133",
+#                traj.names = clustering _name,
 #                viz.dim.red = dim_red,plot.pseudotime = FALSE)
 
-#OR with pseudotime
+#With pseudotime
 VizSmoothedTraj(sce,
-                traj.names = "3.50",
+                traj.names = clustering_name,
                 viz.dim.red = dim_red,plot.pseudotime = TRUE)
 
-#Gene expression
-
-VizFeatureExpression(sce,traj.name = "3.50",
-                     feature.names = "Cd248",
-                     viz.dim.red = dim_red,
-                     plot.traj = TRUE)
 
